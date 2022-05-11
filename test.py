cc025e1358d7e70d4945b396876b95b666e0a659
Yujin-nKim <yujin.kimd@gmail.com>
2022-05-11 15:20:03 +0800

    modify main_blockhandler and main_monitor

--- test.py
+++
@@ -1,7 +0,0 @@
-from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
-
-rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:8332" % ("yujin", "dnlab2022"))
-best_block_hash = rpc_connection.getbestblockhash()
-block = rpc_connection.getblock(best_block_hash)
-print(best_block_hash)
-print(block['height'])

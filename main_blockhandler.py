af723436162233224934da9ec132910c6b4e7fe1
Yujin-nKim <yujin.kimd@gmail.com>
2022-05-18 16:55:04 +0800

    modify main-handler

--- main_blockhandler.py
+++ main_blockhandler.py
@@ -1,5 +1,12 @@
 import argparse
 import zmq
+import mariadb
+import datetime
+from time import strftime
+
+from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
+
+import secret
 
 FLAGS = _ = None
 DEBUG = False
@@ -9,11 +16,33 @@ def message_receiver():
     context = zmq.Context()
     socket = context.socket(zmq.SUB)
     socket.setsockopt_string(zmq.SUBSCRIBE, "")
-    socket.bind("tcp://*:5556")
+    socket.bind("tcp://*:6666")
 
     while True:
         message = socket.recv_json()
         print("Received message : ", message)
+
+        try:
+            conn = mariadb.connect(user=secret.db_username,
+                                   password=secret.db_password,
+                                   host=secret.db_host,
+                                   port=secret.db_port,
+                                   database=secret.db_databasename)
+            cur = conn.cursor()
+
+        except mariadb.Error as e:
+            print(f'Error connecting to MariaDB: {e}')
+            sys.exit(0)
+
+        rpc = AuthServiceProxy("http://%s:%s@127.0.0.1:8332" % (secret.rpc_username, secret.rpc_password))
+        block = rpc.getblock(message['hash'])
+
+        time = datetime.datetime.utcfromtimestamp(block['time']).strftime("%Y-%m-%d %H:%M:%S")
+
+        cur.execute('''INSERT INTO blk VALUES(?, ?, ?);''', (message['height'], message['hash'], time))
+
+        conn.commit()
+        conn.close()
 
 
 def main():
